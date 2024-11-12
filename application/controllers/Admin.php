a731f76413b069ae6aedc184558e12941f1d8b50
Muhamad Farhan <hanzmuha19@gmail.com>
2024-11-12 21:16:35 +0700

    first init github

---
+++ application/controllers/Admin.php
@@ -0,0 +1,39 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+class admin extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model(array('m_user', 'm_barang', 'm_penjualan'));
+        date_default_timezone_set('Asia/Jakarta');
+    }
+    public function index()
+    {
+        if (!$this->session->userdata('level') == 'Admin') {
+            redirect('login');
+        } else {
+            $data['admin'] = $this->m_user->selectAdmin()->row();
+            $data['data'] = $this->m_penjualan->statistikPenjualan()->result();
+            $data['barang'] = $this->m_barang->jumlahBarang()->row();
+            $data['penjualan'] = $this->m_penjualan->jumlahPenjualan()->row();
+            $this->load->view('admin/header', $data);
+            $this->load->view('admin/home');
+            $this->load->view('admin/footer');
+        }
+    }
+
+    public function profil()
+    {
+        $data['admin'] = $this->m_user->selectAdmin()->row();
+        if ($this->session->userdata('level') != 'Admin') {
+            redirect('login');
+        } else {
+            $this->load->view('admin/header', $data);
+            $this->load->view('profil/profil_admin', $data);
+            $this->load->view('admin/footer');
+        }
+    }
+    
+}
+?>
